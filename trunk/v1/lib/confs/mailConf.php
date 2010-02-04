77473311d69d5dd80b403ddba806712cba68cdff
aaishaakhan <aaishaakhan@8f7f7b2c-108a-11df-9b0b-f982558e19cf>
2010-02-04 06:43:22 +0000

    Orangehrm complete core project added on 4th Feb.

---
+++ trunk/v1/lib/confs/mailConf.php
@@ -0,0 +1,14 @@
+
+<?php
+	$this->smtpHost = 'smtp.pucit.edu.pk';
+	$this->smtpUser = 'aisha@pucit.edu.pk';
+	$this->smtpPass = 'astaghfirullah';
+	$this->smtpPort = '25';
+
+	$this->sendmailPath = '/usr/lib/sendmail -ti';
+
+	$this->mailType = 'smtp';
+	$this->mailAddress = 'orangehrm@localhost.localdomain';
+	$this->smtpAuth = 'NONE';
+	$this->smtpSecurity = 'NONE';
+?>
